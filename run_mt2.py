56e36615d3277769d11650fd610b03e1fd847852
Ivan Angelov <iangelov@gmail.com>
2014-08-11 12:49:23 +0200

    Testing eq_all_params for the all three zones

--- run_mt2.py
+++ run_mt2.py
@@ -4,7 +4,8 @@ import model_tools2 as mt
 
 # reimport the module to thest the new changes
 #mt = reload(mt)
-# at the moment it se
+
+
 
 a1 = mt.ang()
 
@@ -16,13 +17,13 @@ a1.ode_eq_add('dot_y_1', '(-b_1 * y_1 - k_1 * delta_L1 + k_2 * (delta_L2 - x_1))
 a1.ode_eq_add('dot_x_2', 'y_2')
 a1.ode_eq_add('dot_y_2', '(-b_2 * y_2 - k_2 * (delta_L2 - x_1)) / m_2')
 
-#for current_key in a1.eq['ode'].keys():
-#	a1.substitute_concept2(current_key, a1.eq['algebraic'])
-
-# the gen_ode_subs method makes exactly the two lines above
+# substitute the algabrain equations in the ode system to generate the complete ode_subs
 a1.gen_ode_subs()
 
-#a1.substitute_concept2('y_1', a1.eq['algebraic'])
-#a1.substitute_concept2('y_2', a1.eq['algebraic'])
 
+# finding all the parameters
+a1.eq_all_params(Zone = 'algebraic')
+a1.eq_all_params(Zone = 'ode')
 a1.eq_all_params(Zone = 'ode_subs')
+
+
