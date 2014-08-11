25258dee3910cc258a5db13fd16db32783afd918
Ivan Angelov <iangelov@gmail.com>
2014-08-11 12:13:20 +0200

    Variables renamed
    delta__L1->delta_L1
    delta__L2->delta_L2

--- run_mt2.py
+++ run_mt2.py
@@ -8,13 +8,13 @@ import model_tools2 as mt
 
 a1 = mt.ang()
 
-a1.eq_add('delta__L1', 'x_1-L_1')
-a1.eq_add('delta__L2', 'x_2-L_2')
+a1.eq_add('delta_L1', 'x_1-L_1')
+a1.eq_add('delta_L2', 'x_2-L_2')
 
 a1.ode_eq_add('x_1', 'y_1')
-a1.ode_eq_add('y_1', '(-b_1 * y_1 - k_1 * delta__L1 + k_2 * (delta__L2 - x_1)) / m_1')
+a1.ode_eq_add('y_1', '(-b_1 * y_1 - k_1 * delta_L1 + k_2 * (delta_L2 - x_1)) / m_1')
 a1.ode_eq_add('x_2', 'y_2')
-a1.ode_eq_add('y_2', '(-b_2 * y_2 - k_2 * (delta__L2 - x_1)) / m_2')
+a1.ode_eq_add('y_2', '(-b_2 * y_2 - k_2 * (delta_L2 - x_1)) / m_2')
 
 #for current_key in a1.eq['ode'].keys():
 #	a1.substitute_concept2(current_key, a1.eq['algebraic'])
