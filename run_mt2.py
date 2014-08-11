90cb049b0ef58a7272f24d58a63716f19168b202
Ivan Angelov <iangelov@gmail.com>
2014-08-11 12:41:49 +0200

    Variables renamed
    The LHS of the ode system start now with dot_

--- run_mt2.py
+++ run_mt2.py
@@ -11,10 +11,10 @@ a1 = mt.ang()
 a1.eq_add('delta_L1', 'x_1-L_1')
 a1.eq_add('delta_L2', 'x_2-L_2')
 
-a1.ode_eq_add('x_1', 'y_1')
-a1.ode_eq_add('y_1', '(-b_1 * y_1 - k_1 * delta_L1 + k_2 * (delta_L2 - x_1)) / m_1')
-a1.ode_eq_add('x_2', 'y_2')
-a1.ode_eq_add('y_2', '(-b_2 * y_2 - k_2 * (delta_L2 - x_1)) / m_2')
+a1.ode_eq_add('dot_x_1', 'y_1')
+a1.ode_eq_add('dot_y_1', '(-b_1 * y_1 - k_1 * delta_L1 + k_2 * (delta_L2 - x_1)) / m_1')
+a1.ode_eq_add('dot_x_2', 'y_2')
+a1.ode_eq_add('dot_y_2', '(-b_2 * y_2 - k_2 * (delta_L2 - x_1)) / m_2')
 
 #for current_key in a1.eq['ode'].keys():
 #	a1.substitute_concept2(current_key, a1.eq['algebraic'])
